64c331cd76238b92e600c45f44c607baf1a0c567
bfejwndbdu <officiallyrichard@gmail.com>
2025-06-14 20:24:37 +0800

    Imported SFX &, BGM. Door SFX

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -12,6 +12,8 @@ public class DoorBehaviour : MonoBehaviour
     private Vector3 closedRotation;
     //Variable for vectors for when open
     private Vector3 openRotation;
+    AudioSource doorOpenAudio;
+    AudioSource doorCloseAudio;
 
     void Start()
     {
@@ -19,18 +21,23 @@ public class DoorBehaviour : MonoBehaviour
         closedRotation = transform.eulerAngles;
         //Add 90 degrees rotation to open door
         openRotation = closedRotation + new Vector3(0, 90, 0);
+        doorOpenAudio = GetComponent<AudioSource>();
     }
     public void Interact()
     {
         //Check whether Door is open or closed
         if (!isOpen)
         {
+            //Play Open audio
+            doorOpenAudio.Play();
             //Set transform vectors to the open vectors
             transform.eulerAngles = openRotation;
             isOpen = true;
         }
         else
         {
+            //Play Close audio
+            doorCloseAudio.Play();
             //Set transform vectors to the closed vectors
             transform.eulerAngles = closedRotation;
             isOpen = false;
